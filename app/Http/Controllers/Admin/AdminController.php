5afd050ed74444991e11e8d14ef48a51aa2999e3
Ftittawat <tittawatjaiou@gmail.com>
2022-05-14 09:08:13 +0700

    update join&show data in database

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,27 +4,39 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Student;
+use App\Models\Teacher;
+use App\Models\CourseDetail;
+use App\Models\ClassDetail;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     function dashboard(){
-
         $users = User::all();
-
         return view('admin.users.dashboard', compact('users'));
         
     }
 
     function studentManage(){
-        return view('admin.manage.student');
+        $students = Student::all();
+        return view('admin.manage.student',compact('students'));
     }
 
     function teacherManage(){
-        return view('admin.manage.teacher');
+        $teachersinfo = Teacher::Join('departments', 'teachers.DepartmentID', '=', 'departments.DepartmentID')
+        ->select('teachers.TeacherID','teachers.TeacherName','teachers.Email','departments.DepartmentName','departments.FacultyName')
+        ->get();
+        return view('admin.manage.teacher',compact('teachersinfo'));
     }
 
     function courseManage(){
-        return view('admin.manage.course');
+        $courseinfo = CourseDetail::Join('departments', 'course_details.DepartmentID', '=', 'departments.DepartmentID')
+        ->select('course_details.CourseID','course_details.CourseName','course_details.Credit','departments.DepartmentName','departments.FacultyName')
+        ->get();
+        $classinfo = ClassDetail::Join('course_details', 'course_details.CourseID', '=', 'class_details.CourseID')
+        ->select('course_details.CourseID','course_details.CourseName','class_details.ClassID','class_details.Section','class_details.Semester')
+        ->get();
+        return view('admin.manage.course',compact('courseinfo','classinfo'));
     }
 }
