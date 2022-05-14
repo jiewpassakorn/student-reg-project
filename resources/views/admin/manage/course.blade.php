b9ed33183ec4dbfacac0304eb24f17b3a0e0458c
jiewpassakorn <passakornjiew@gmail.com>
2022-05-15 04:38:09 +0700

    update

--- resources/views/admin/manage/course.blade.php
+++ resources/views/admin/manage/course.blade.php
@@ -21,6 +21,28 @@
                 <a href="#insertModal"><button class="btn ms-sm-5 mx-2 btn-success" data-bs-toggle="modal" data-bs-target="#insertCourseModal">เพิ่มวิชาเรียน</button></a>
             </div>
             <table class="table table-striped shadow-sm text-center mt-3">
+                {{-- alert message --}}
+                @if(Session::has('success'))
+                <div class="d-inline-flex">
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:">
+                            <use xlink:href="#check-circle-fill" />
+                        </svg>
+                        {{Session::get('success')}}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                </div>
+                @elseif(Session::has('delete'))
+                <div class="d-inline-flex">
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:">
+                            <use xlink:href="#exclamation-triangle-fill" />
+                        </svg>
+                        {{Session::get('delete')}}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                </div>
+                @endif
                 <thead class="table table-dark">
                     <tr>
                         <th>รหัสวิชา</th>
@@ -50,52 +72,6 @@
             {{$courseinfo->links()}}
         </div>
 
-        {{-- alert message --}}
-        @if(Session::has('success'))
-        <div class="d-inline-flex">
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
-                {{Session::get('success')}}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>                  
-        </div>
-        @elseif(Session::has('delete'))      
-            <div class="d-inline-flex">
-                <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
-                    {{Session::get('delete')}}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-            </div>
-        @endif
-
-        <table class="table table-striped shadow-sm text-center mt-3">
-            <thead class="table table-dark">
-                <tr>
-                    <th>รหัสวิชา</th>
-                    <th>ชื่อวิชา</th>
-                    <th>หน่วยกิต</th>
-                    <th>ภาควิชา</th>
-                    <th>จำนวนกลุ่ม</th>
-                    <th>แก้ไขข้อมูล</th>
-                    <th>ลบข้อมูล</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($courseinfo as $row)
-                <tr>
-                    <th>{{$row->CourseID}}</th>
-                    <td>{{$row->CourseName}}</td>
-                    <td>{{$row->Credit}}</td>
-                    <td>{{$row->DepartmentName}}</td>
-                    <td>{{$classinfo->where('CourseID',$row->CourseID)->count()}}</td>
-                    <td><a href="#"><button class="btn btn-info" data-bs-toggle="modal" data-bs-target="#editModal">แก้ไขข้อมูล</button></a> </td>
-                    <td><a href="#"><button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal">ลบข้อมูล</button></a> </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-        {{$courseinfo->links()}}
     </div>
 </div>
 <!--Container Main end-->
@@ -114,48 +90,30 @@
                         @csrf
                         <div class="row">
                             <div class="col-md-12 mt-2"><label class="labels">รหัสวิชา</label>
-                            @error('CourseID')<span class="text-danger py-0">({{$message}})</span>@enderror
-                            <input name="CourseID" type="text" class="form-control" placeholder="" value=""></div>
+                                @error('CourseID')<span class="text-danger py-0">({{$message}})</span>@enderror
+                                <input name="CourseID" type="text" class="form-control" placeholder="" value="">
+                            </div>
                             <div class="col-md-6 mt-2"><label class="labels">ชื่อวิชา</label>
-                            @error('CourseName')<span class="text-danger py-0">({{$message}})</span>@enderror
-                            <input name="CourseName" type="text" class="form-control" placeholder="" value=""></div>
+                                @error('CourseName')<span class="text-danger py-0">({{$message}})</span>@enderror
+                                <input name="CourseName" type="text" class="form-control" placeholder="" value="">
+                            </div>
                             <div class="col-md-6 mt-2"><label class="labels">หน่วยกิต</label>
-                            @error('Credit')<span class="text-danger py-0">({{$message}})</span>@enderror
-                            <input name="Credit"type="text" class="form-control" value="" placeholder=""></div>
+                                @error('Credit')<span class="text-danger py-0">({{$message}})</span>@enderror
+                                <input name="Credit" type="text" class="form-control" value="" placeholder="">
+                            </div>
                         </div>
                         <div class="row">
                             <div class="col-md-12 mt-2"><label class="labels">ภาควิชา</label>
-                            @error('DepartmentID')<span class="text-danger py-0">({{$message}})</span>@enderror
-                            <input name="DepartmentID" type="text" class="form-control" placeholder="" value=""></div>
+                                @error('DepartmentID')<span class="text-danger py-0">({{$message}})</span>@enderror
+                                <input name="DepartmentID" type="text" class="form-control" placeholder="" value="">
+                            </div>
                         </div>
                         <div class="modal-footer mt-3">
                             <input type="submit" value="Save Profile" class="btn btn-primary profile-button add_button">
                         </div>
                     </form>
                 </div>
-                <div class="modal-body">
-                    <div class="col-md-12">
-                            <div class="row">
-                                <div class="col-md-12 mt-2"><label class="labels">รหัสวิชา</label><input type="text" class="form-control" placeholder="" value=""></div>
-                                <div class="col-md-6 mt-2"><label class="labels">ชื่อวิชา</label><input type="text" class="form-control" placeholder="" value=""></div>
-                                <div class="col-md-6 mt-2"><label class="labels">หน่วยกิต</label><input type="text" class="form-control" value="" placeholder=""></div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-12 mt-2"><label class="labels">ภาควิชา</label>
-                                    <select name="DepartmentID" class="form-select">
-                                        <option selected>Choose department...</option>
-                                        <option value="101">Computer Engineering</option>
-                                        <option value="102">ME</option>
-                                        <option value="111">Maths</option>
-                                    </select>
-                                </div>
-                            </div>
-                    </div>
-                </div>
-            </div>
-            <div class="modal-footer mt-3">
-                <button class="btn btn-secondary" data-bs-dismiss="modal">ยกเลิก</button>
-                <button class="btn btn-success">บันทึกข้อมูล</button>
+
             </div>
         </div>
     </div>
