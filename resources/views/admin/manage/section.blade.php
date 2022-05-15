327ee6604da10f01ffa2cad440ceec8abbe667b4
gkana19 <tonza9yi@gmail.com>
2022-05-16 00:12:18 +0700

    Update section.blade.php

--- resources/views/admin/manage/section.blade.php
+++ resources/views/admin/manage/section.blade.php
@@ -53,8 +53,6 @@
                     <th>ชื่อวิชา</th>
                     <th>กลุ่ม</th>
                     <th>ภาคการศึกษา</th>
-                    <th>อาจารย์ผู้สอน</th>
-                    <th>จำนวนนักศึกษา</th>
                     <th>แก้ไข</th>
                 </tr>
             </thead>
@@ -66,8 +64,10 @@
                     <td>{{$row->CourseName}}</td>
                     <td>{{$row->Section}}</td>
                     <td>{{$row->Semester}}</td>
-                    <td><a href="#"><button class="btn ms-sm-5 mx-2 btn-info" data-bs-toggle="modal" data-bs-target="#editModal">แก้ไขข้อมูล</button></a> </td>
-                    <td><a onclick="return confirm('ยืนยันที่จะลบ คลาส {{$row->ClassID}} รายวิชา {{$row->CourseName}}')" href="{{url('/admin/SectionManage/delete/'.$row->ClassID)}}"><button class="btn ms-sm-5 mx-2 btn-danger" >ลบข้อมูล</button></a></td>
+                    <td>
+                        <a href="#"><button class="btn btn-info" data-bs-toggle="modal" data-bs-target="#editModal">แก้ไขข้อมูล</button></a>
+                        <a onclick="return confirm('ยืนยันที่จะลบ คลาส {{$row->ClassID}} รายวิชา {{$row->CourseName}}')" href="{{url('/admin/SectionManage/delete/'.$row->ClassID)}}"><button class="btn btn-danger" >ลบข้อมูล</button></a>
+                    </td>
 
                 @endforeach
             </tbody>
